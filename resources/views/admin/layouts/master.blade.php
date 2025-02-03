6369b0ee20511ab225d6fd194c8b0cef9fcff80c
hieucodekk <hieudo149204@gmail.com>
2025-02-04 00:13:10 +0700

    không có gì

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from zoyothemes.com/tapeli/html/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Jul 2024 08:33:02 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Dashboard | Tapeli - Responsive Admin Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
+    <meta name="author" content="Zoyothemes" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    @include('admin.layouts.partials.css')
+
+</head>
+
+<!-- body start -->
+
+<body data-menu-color="light" data-sidebar="default">
+
+    <!-- Begin page -->
+    <div id="app-layout">
+
+
+        <!-- Topbar Start -->
+        @include('admin.layouts.componets.topbar')
+        <!-- end Topbar -->
+
+        <!-- Left Sidebar Start -->
+        @include('admin.layouts.componets.leftbar')
+        <!-- Left Sidebar End -->
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+
+                <!-- Start Content-->
+                @yield('content')
+                <!-- container-fluid -->
+            </div> <!-- content -->
+
+            <!-- Footer Start -->
+           @include('admin.layouts.componets.footer')
+            <!-- end Footer -->
+
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+    </div>
+    <!-- END wrapper -->
+
+    @include('admin.layouts.partials.js')
+
+</body>
+
+<!-- Mirrored from zoyothemes.com/tapeli/html/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Jul 2024 08:34:03 GMT -->
+
+</html>
---
+++ resources/views/admin/layouts/partials/css.blade.php
@@ -0,0 +1,8 @@
+<!-- App favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+<!-- App css -->
+<link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+<!-- Icons -->
+<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
