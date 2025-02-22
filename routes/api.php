caa28a4daa6d9584fc020007a01fb4100cbdedd1
Thi27 <thi54792@gmail.gmailcom>
2025-02-22 13:32:01 +0700

    crud comments

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\CommentController;
-use App\Models\Comment;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\UserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +23,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Đăng nhập
+Route::post('/login', [LoginController::class, 'login'])->name('api.login');
+
+// Các route yêu cầu xác thực
+Route::middleware('auth:sanctum')->group(function () {
+    // Đăng xuất
+    Route::post('/logout', [LogoutController::class, 'logout'])->name('api.logout');
+ 
+    // Lấy thông tin người dùng
+    // Route::get('/user', [UserController::class, 'getUser'])->name('api.user');
+});
+
 Route::resource('comments', CommentController::class);
