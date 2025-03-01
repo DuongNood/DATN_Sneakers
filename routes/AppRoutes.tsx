2a4125b41837270f19ec359c0b3999b6dfcc5c61
Đào Văn Quang <59763661+daovanquang2000@users.noreply.github.com>
2025-03-01 21:07:40 +0700

    Router

---
+++ routes/AppRoutes.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Layout from "../layouts/MainLayout";
+import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword";
+import NotFound from "../pages/NotFound";
+import NewsPage from "../pages/NewsPage";
+import DetailsProduct from "../pages/DetailsProduct";
+import DetailsNew from "../pages/DetailsNew";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/details-product" element={<DetailsProduct />} />
+        <Route path="/details-new" element={<DetailsNew />} />
+      </Route>
+    </Routes>
+  );
+}
