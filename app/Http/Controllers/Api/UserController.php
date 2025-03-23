c152c90ed5011f085ec00c3264d88519b2637794
doanhhao <163215654+Angostura483@users.noreply.github.com>
2025-03-23 21:28:45 +0700

    Api update User

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -38,7 +40,45 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        
+        $data = $request->validated();
+
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Không tồn tại bản ghi có ID là: ' . $id
+            ], 404);
+        }
+
+        try {
+            if ($request->hasFile('image_user')) {
+                $data['image_user'] = Storage::put('users', $request->file('image_user'));
+            }
+
+            $currentImage = $user->image_user;
+
+            $user->update($data);
+
+            if ($request->hasFile('image_user') && !empty($currentImage) && Storage::exists($currentImage)) {
+                Storage::delete($currentImage);
+            }
+
+            return response()->json($user, 201);
+        } catch (\Throwable $th) {
+
+            if (!empty($data['image_user']) && Storage::exists($data['image_user'])) {
+                Storage::delete($data['image_user']);
+            }
+
+            Log::error(
+                __CLASS__ . '@' . __FUNCTION__,
+                ['error' => $th->getMessage()]
+            );
+
+            return response()->json([
+                'message' => 'Lỗi hệ thống'
+            ], 500);
+        }
     }
 
     /**
