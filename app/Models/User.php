92b7d6b37dc30f7639b1c572df23d6e932a12dda
doanhhao <163215654+Angostura483@users.noreply.github.com>
2025-03-28 09:04:27 +0700

    Cập nhật giao diện Boostrap + code logic cho Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function role()
     {
-        return $this->belongsTo(Role::class);
+        return $this->belongsTo(Role::class, 'role_id');
     }
 
     public function isAdmin()
