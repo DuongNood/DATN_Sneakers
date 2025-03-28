cca85465d5856b4cd1348572237d96554b367025
doanhhao <163215654+Angostura483@users.noreply.github.com>
2025-03-28 23:07:55 +0700

    Update index.blade.php

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -16,6 +16,7 @@
                 Thao tác thành công
             </div>
         @endif
+
         <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
             <div class="flex-grow-1">
                 <h4 class="fs-18 fw-semibold m-0">Danh sách người dùng</h4>
