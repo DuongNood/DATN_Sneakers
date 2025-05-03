f094ce2ff5e342da6358370b82272833adcc095a
HuuDucck1 <dduc8449@gmail.com>
2025-05-03 22:05:38 +0700

    sửa quên mật khẩu, gửi mail

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -27,7 +27,7 @@ class ForgotPasswordController extends Controller
         $user = User::where('email', $request->email)->first();
 
 
-        // Tạo mật khẩu tạm thời
+        // Tạo mật khẩu tạm thời, random 8 kí tự 
         $temporaryPassword = Str::random(8);
         $user->password = Hash::make($temporaryPassword);
         $user->save();
@@ -35,11 +35,11 @@ class ForgotPasswordController extends Controller
         // Gửi email chứa mật khẩu tạm thời
         Mail::send('admin.emails.reset-password', ['user' => $user, 'temporaryPassword' => $temporaryPassword], function ($message) use ($user) {
             $message->to($user->email);
-            $message->subject('Khôi phục mật khẩu');
+            $message->subject('Khôi phục mật khẩu.');
         });
 
         return response()->json([
-            'message' => 'Mật khẩu mới đã được gửi đến email của bạn.'
+            'message' => 'Mật khẩu mới đã được gửi đến email của bạn, vui lòng kiểm tra hộp thư đến.',
         ], 200);
     }
 }
